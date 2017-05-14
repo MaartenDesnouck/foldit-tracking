6ff354ee18206105a1aa8c3597b4281ce52dc0ec
Maarten Desnouck <maarten.desnouck@gmail.com>
2017-05-14 17:10:20 +0200

    include all the files

--- google-apps-script-projects/FOLDIT_SOLO/gas-include.js
+++ google-apps-script-projects/FOLDIT_SOLO/gas-include.js
@@ -1,13 +1,13 @@
 // Use npm google-apps-script to inlcude these files
-'getOldPlayerList.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/getOldPlayerList.js'
-'getWebpage.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/getWebpage.js'
-'refreshRanking.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/refreshRanking.js'
-'sortHistory.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/sortHistory.js'
-'updateChartLegend.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/updateChartLegend.js'
-'updateHistory.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/updateHistory.js'
-'updatePlayerList.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/updatePlayerList.js'
-'list.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/list.js'
-'split.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/split.js'
+'getOldPlayerList.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/getOldPlayerList.js'
+'getWebpage.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/getWebpage.js'
+'refreshRanking.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/refreshRanking.js'
+'sortHistory.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/sortHistory.js'
+'updateChartLegend.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/updateChartLegend.js'
+'updateHistory.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/updateHistory.js'
+'updatePlayerList.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/updatePlayerList.js'
+'list.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/list.js'
+'split.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/foldit-tracking/master/include/split.js'
 
 // Cross project libraries
 'time.js' - 'https://raw.githubusercontent.com/MaartenDesnouck/google-apps-script-include/master/Time.js'
