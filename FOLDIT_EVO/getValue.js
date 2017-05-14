df3ea8c660ed095b3693371afdbf2ab9922c6b88
Maarten Desnouck <maarten.desnouck@gmail.com>
2017-05-14 15:31:33 +0200

    Switch FOLDIT_EVO to use gas-include

--- FOLDIT_EVO/getValue.js
+++
@@ -1,9 +0,0 @@
-function getValue(row, kolom, blad) {
-    var data = blad.getDataRange().getValues();
-
-    if (blad.getDataRange().getNumRows() < row + 1) {
-        return undefined;
-    } else {
-        return data[row][kolom];
-    }
-}
